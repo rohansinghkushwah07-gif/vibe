2f5f5bc16705f249f0340c5f4ab92cec2d688612
Rohan Kushwah <rohansinghkushwah07@gmail.com>
2026-03-11 00:09:57 +0530

    05:ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,34 @@
 
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@base-ui/react";
+import { Input } from "@base-ui/react";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
-const page = () => {
+export default function Page() {
+  const [value, SelectValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions ({
-    onSuccess : () => {
-      toast.success("Background job started")
-    }
-  }));
+
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    })
+  );
+
   return (
     <div className="p-4 max-w-7xl">
-        <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text : "john"})}>
-          invoke Background job
-        </Button>
+      <Input value={value} onChange={(e) => SelectValue(e.target.value)} />
+
+      <Button
+        className="bg-blue-600 text-white px-4 py-2 rounded-md mt-2"
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ value: value })}
+      >
+        invoke Background job
+      </Button>
     </div>
   );
 }
-
-
-export default page;
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,14 +1,22 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
+  async ({ event }) => {
 
-     await step.sleep("wait-a-moment", "10s");
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert Next.js developer. You write readable and maintainable code. You write simple Next.js and React snippets.",
+      model: openai({ model: "gpt-4o" }),
+    });
 
-      await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    const { output } = await codeAgent.run(
+      `Write a Next.js or React snippet for: ${event.data.value}`
+    );
+
+    return { output };
+  }
 );
